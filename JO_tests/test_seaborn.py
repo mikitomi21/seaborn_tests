928a98e3780304b26725bbac19b1dc24823e6c7a
mikitomi21 <kubastach2001@gmail.com>
2024-03-20 17:18:50 +0100

    Add install and uninstall tests

--- JO_tests/test_seaborn.py
+++ JO_tests/test_seaborn.py
@@ -2,6 +2,23 @@ import pytest
 import seaborn as sns
 import matplotlib
 import pandas
+import subprocess
+
+
+def test_library_installation():
+    command = "pip install seaborn"
+    success = ["Installing collected packages: seaborn", "Requirement already satisfied: seaborn"]
+
+    output = subprocess.run(command, shell=True, capture_output=True, text=True)
+    assert any(message in output.stdout for message in success)
+
+
+def test_library_uninstallation():
+    command = "pip uninstall seaborn -y"
+    success = ["Successfully uninstalled", "Skipping seaborn as it is not installed"]
+
+    output = subprocess.run(command, shell=True, capture_output=True, text=True)
+    assert any(message in output.stdout for message in success)
 
 
 def test_load_diamonds_dataset():
